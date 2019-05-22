d550d4335faa7bba256d8f2e5700a96154280656
Alexandre Gervais <algerli@hotmail.com>
2019-05-22 17:15:28 +0200

    Mister Cocktail a lil' beyond, but within basics

--- db/seeds.rb
+++ db/seeds.rb
@@ -10,14 +10,20 @@ require 'json'
 Ingredient.create(name: "lemon")
 Ingredient.create(name: "ice")
 Ingredient.create(name: "mint leaves")
+Ingredient.create(name: "whisky")
+Ingredient.create(name: "gin")
+Ingredient.create(name: "vodka")
+Ingredient.create(name: "rhum")
+Ingredient.create(name: "mapple syrup")
+Ingredient.create(name: "strawberry syrup")
 
 Cocktail.create(name: "Negroni")
 Cocktail.create(name: "Dry Martini")
 Cocktail.create(name: "Manhattan")
 
 
-url = "https://www.thecocktaildb.com/api/json/v1/1/list.php?i=list"
+# url = "https://www.thecocktaildb.com/api/json/v1/1/list.php?i=list"
 
-ingredients = File.read(url)
+# ingredients = File.read(url)
 
-ingredients = JSON.parse(serialized_ingredients)
+# ingredients = JSON.parse(serialized_ingredients)
